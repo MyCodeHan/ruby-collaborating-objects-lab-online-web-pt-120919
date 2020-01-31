0bef31e582cfebca6febf52d64921832a7120ada
Hannah Harrington <hannahharrington@outlook.com>
2020-01-31 23:10:46 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,23 @@
+require "pry"
+class MP3Importer
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    #only want .mp3 files
+    @files = []
+    file = Dir.entries(@path)
+    file.each do |file|
+      if file.include?("mp3")
+        @file << file 
+      binding.pry
+    end
+  end
+  @file
+  def import
+    files.each { |file| Song.new_by_filename(file) }
+  end
+end
